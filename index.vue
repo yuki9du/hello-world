68c574b0d109a1f3626f831e9fb62cf9abcf12dd
yuki9du <55126239+yuki9du@users.noreply.github.com>
2019-09-18 15:50:08 +0800

    1
    2

---
+++ index.vue
@@ -0,0 +1,45 @@
+<template>
+    <todo-list :todos="todos" class="aaa">
+        <span  slot-scope="lyc"> {{lyc.wpp}}</span>
+    </todo-list>
+</template>
+
+<script>
+import todoList from './todoList'
+export default {
+	components: {
+		todoList
+	},
+	data() {
+		return {
+			todos: [
+				{
+					id: 0,
+					text: 'ziwei0',
+					isComplete: false
+				},
+				{
+					text: 'ziwei1',
+					id: 1,
+					isComplete: true
+				},
+				{
+					text: 'ziwei2',
+					id: 2,
+					isComplete: false
+				},
+				{
+					text: 'ziwei3',
+					id: 3,
+					isComplete: false
+				}
+			]
+		}
+	}
+}
+</script>
+<style>
+.aaa{
+  color: red !important;
+}
+</style>
---
+++ todoList.vue
@@ -0,0 +1,22 @@
+<template>
+    <ul>
+        <li v-for="todo in todos" :key="todo.id">
+            <slot :wpp="todo"></slot>
+            <p>{{todo}}</p>
+        </li>
+    </ul>
+</template>
+
+<script>
+export default {
+props:{
+    todos:{
+        type:Array
+    }
+}
+}
+</script>
+
+<style>
+
+</style>
